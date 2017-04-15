05d4946ca6647677e3002a4169f9a2c617054939
David <davidventura27@gmail.com>
2017-04-15 19:21:41 -0300

    change dict keys to ints, filter file extensions

---
+++ channel_indexer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import os
+
+BASEPATH = "/home/david/git/old-tv/channels/"
+ADS_PATH = "/home/david/git/old-tv/ads/"
+
+
+def get_directories(path):
+    ret = []
+    for f in os.listdir(path):
+        fullpath = os.path.join(path, f)
+        if os.path.isdir(fullpath):
+            ret.append(f)
+
+    return ret
+
+
+def valid_ext(path):
+    return (path.endswith("mp4") or path.endswith("mkv"))
+
+
+def get_files(path):
+    ret = []
+    for f in os.listdir(path):
+        fullpath = os.path.join(path, f)
+        if os.path.isfile(fullpath) and valid_ext(fullpath):
+            ret.append(f)
+
+    # Sorting the output is very important
+    # As the playback will be ordered.
+    return sorted(ret)
+
+
+def index():
+    channels = get_directories(BASEPATH)
+    data = {}
+    for c in channels:
+        data[int(c)] = {}
+        channel_path = os.path.join(BASEPATH, c)
+        for p in get_directories(channel_path):
+            program_path = os.path.join(channel_path, p)
+            data[int(c)][int(p)] = get_files(program_path)
+
+    ads = get_files(ADS_PATH)
+    return {'ads': ads, 'channels': data}
+
+
+if __name__ == '__main__':
+    print(index())
