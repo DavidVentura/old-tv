9c62a7b694a8d16892d4deec27e1889765ddb99a
David <davidventura27@gmail.com>
2017-04-17 15:52:42 -0300

    add basic channel switching. needs to be more robust

--- main.py
+++ main.py
@@ -24,16 +24,18 @@ class TrackProgram():
         s = self.get_current_status()
         ret = ""
         if s['curType'] == 'ad':
+            s['curFileIndex'] = s['curFileIndex'] % len(self.guide['ads'])
             ret = os.path.join(self.ADS_PATH,
                                self.guide['ads'][s['curFileIndex']])
         else:
+            self.program = self.program % len(self.guide['channels'][self.channel])
+            s['curFileIndex'] = s['curFileIndex'] % len(self.guide['channels'][self.channel][self.program])
             ret = os.path.join(self.BASEPATH,
                                str(self.channel),
                                str(self.program),
                                self.guide['channels'][self.channel][self.program][s['curFileIndex']])
 
         return "file://" + ret
-
 
     def get_next_file(self):
         if self.get_current_status()['curType'] == 'ad':
@@ -65,6 +67,10 @@ class TrackProgram():
                 self.player.channel()
             elif data.startswith("uri") or data == "u":
                 self.player.set_next_file(self.get_random_ad())
+            elif data == "+":
+                self.set_channel(self.channel + 1)
+            elif data == "-":
+                self.set_channel(self.channel - 1)
             else:
                 try:
                     self.player.seek(float(data))
@@ -85,6 +91,14 @@ class TrackProgram():
     def load_status(self):
         # TODO: Load this from a file
         return {
+                2: {
+                    'curType': 'ad',
+                    'curFileIndex': 5,
+                    'curFileTime': 10,
+                    'curFileDuration': 40,
+                    'lastChapterIndex': 3,
+                    'lastProgramIndex': 0,
+                   },
                 3: {
                     'curType': 'program',
                     'curFileIndex': 5,
@@ -92,19 +106,37 @@ class TrackProgram():
                     'curFileDuration': 40,
                     'lastChapterIndex': 3,
                     'lastProgramIndex': 0,
-                   }
+                   },
+                5: {
+                    'curType': 'program',
+                    'curFileIndex': 5,
+                    'curFileTime': 10,
+                    'curFileDuration': 40,
+                    'lastChapterIndex': 3,
+                    'lastProgramIndex': 0,
+                   },
                 }
 
-    def __init__(self):
-        self.guide = ci.index()
-        self.status = self.load_status()
-        self.player = Player(on_finished=self.finished_playing)
+    def set_channel(self, channel):
+        self.channel = min(max(channel, 1), 12)
+        print("New channel:", self.channel)
+        if self.channel not in self.valid_channels:
+            self.player.snow()
+            return
 
-        print("Resuming file", self.get_cur_file())
         self.player.set_next_file(self.get_cur_file())
         cs = self.get_current_status()
         self.player.change_uri(start_time=cs['curFileTime'],
                                duration=cs['curFileDuration'])
+
+    def __init__(self):
+        self.guide = ci.index()
+        self.valid_channels = self.guide['channels'].keys()
+
+        self.status = self.load_status()
+        self.player = Player(on_finished=self.finished_playing)
+        self.set_channel(3)  # FIXME: Must call a valid channel so pads link
+        self.set_channel(4)
 
         t1 = threading.Thread(target=self.control)
         t1.start()
