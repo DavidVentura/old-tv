6ae596ba378cc69ef55bd756197bc0bc39e6c7ab
David <davidventura27@gmail.com>
2017-04-20 21:45:02 -0300

    load and save state

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 #!/usr/bin/python3
 from player import Player
+import cache
 import os
 import threading
 import random
 import channel_indexer as ci
 import gpio
+import json
+import time
 from pprint import pprint
 
+
+def list_saves():
+    ret = []
+    for f in os.listdir(ci.cwd):
+        if f.endswith('json') and os.path.getsize(f) > 5:
+            # Consider the possibility of a file writing '{}'
+            ret.append(f)
+    return ret
+
+
+def newest_file(xs):
+    ltime = None
+    ret = None
+    for f in xs:
+        t = os.path.getmtime(f)
+        if ltime is None or t > ltime:
+            # print("%s is newer than %s" % (f, ret))
+            ltime = t
+            ret = f
+    return ret
 
 def sanitize_status(s, guide):
     status = s
     channels = guide['channels']
     ads = guide['ads']
-    for channel in status.keys():
+    for channel in channels.keys():
+        if channel not in status:
+            status[channel] = {}
+
         if 'curType' not in status[channel]:
             status[channel]['curType'] = 'program'
 
@@ -29,14 +55,20 @@ def sanitize_status(s, guide):
             status[channel]['curFileIndex'] = status[channel]['curFileIndex']\
                 % len(ads)
         else:
-            print(channel, status[channel])
             status[channel]['curProgram'] = status[channel]['curProgram']\
                 % len(channels[channel])
+
             status[channel]['curFileIndex'] = status[channel]['curFileIndex']\
                 % len(channels[channel][status[channel]['curProgram']])
 
         if 'lastChapter' not in status[channel]:
             status[channel]['lastChapter'] = {}
+
+        keys = [k for k in status[channel]['lastChapter'].keys()]
+        for k in keys:
+            if type(k) == str:
+                status[channel]['lastChapter'][int(k)] = status[channel]['lastChapter'][k]
+                del status[channel]['lastChapter'][k]
 
         for program in channels[channel]:
             if program not in status[channel]['lastChapter']:
@@ -45,32 +77,34 @@ def sanitize_status(s, guide):
     return status
 
 
-def load_status():
-    # TODO: Load this from a file
-    return {
-            2: {
-                'curType': 'ad',
-                'curFileIndex': 5,
-                'curFileTime': 10,
-                'curFileDuration': 40,
-                'lastProgramIndex': 0,
-               },
-            4: {
-                'curType': 'program',
-                'curProgram': 0,
-                'curFileTime': 10,
-                'curFileDuration': 40,
-                'lastProgramIndex': 0,
-               },
-            5: {
-                'curType': 'program',
-                'curProgram': 0,
-                'curFileTime': 10,
-                'curFileDuration': 40,
-                'lastProgramIndex': 0,
-               },
-            8: {}
-            }
+def keys_to_int(obj):
+    keys = obj.keys()
+    for k in keys:
+        if type(k) == str:
+            obj[int(k)] = obj[k]
+            del obj[k]
+    return obj
+
+
+def load_status(fname):
+    data = {}
+    try:
+        with open(fname, 'r') as f:
+            data = json.loads(f.read())
+            data = keys_to_int(data)
+    except Exception as e:
+        print(e)
+
+    return data
+
+
+def save_status(status):
+    seconds = str(int(round(time.time())))
+    path = os.path.join(ci.cwd, seconds + ".json")
+    print("Saving status to ", path)
+    pprint(status)
+    with open(path, "w") as f:
+        f.write(json.dumps(status, sort_keys=False, indent=4))
 
 
 class TrackProgram():
@@ -124,7 +158,7 @@ class TrackProgram():
 
     def control(self):
         data = ""
-        while data != "q":
+        while True:
             data = input()
             if data == "s":
                 pprint(self.status)
@@ -132,12 +166,14 @@ class TrackProgram():
                 self.set_channel(self.channel + 1)
             elif data == "-":
                 self.set_channel(self.channel - 1)
+            elif data == "q":
+                self.set_current_status('curFileTime', self.player.get_cur_time())
+                save_status(self.status)
             else:
                 try:
                     self.player.seek(float(data))
                 except Exception:
-                    print("Exiting control")
-                    break
+                    print("Invalid")
 
     def get_current_status(self):
         # TODO: Return valid data on incomplete input
@@ -228,7 +264,11 @@ class TrackProgram():
         # FIXME: Sorts... poorly
         self.valid_channels = self.guide['channels'].keys()
 
-        self.status = sanitize_status(load_status(), self.guide)
+        statusfile = ''
+        statusfile = os.path.join(ci.cwd, "1492733314.json")
+        statusfile = newest_file(list_saves())
+        loaded = load_status(statusfile)
+        self.status = sanitize_status(loaded, self.guide)
 
         self.player = Player(blank_uri="file://" + self.BLANK_PATH,
                              on_finished=self.finished_playing,
@@ -238,6 +278,7 @@ class TrackProgram():
         t1 = threading.Thread(target=self.control)
         t1.daemon = True
         t1.start()
+        self.set_channel(4)
         self.player.run()
         g.stop()
         t1.join()
