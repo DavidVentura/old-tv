e01d7c5e6d975ec1a16e00a8f90ba648ae49eeb4
David <davidventura27@gmail.com>
2017-05-25 13:05:14 -0300

    update script

--- noise/make_noise.sh
+++ noise/make_noise.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
- timeout -s int 10 gst-launch-1.0 -e videotestsrc is-live=true pattern=snow ! video/x-raw,width=640,height=480,framerate=25/1 ! x264enc psy-tune=grain qp-max=30 ! qtmux name=q ! filesink location=noise.mp4 audiotestsrc wave=white-noise volume=0.02 is-live=true ! voaacenc ! q.
+rm -f noise.mp4
+timeout -s int 10 \
+	gst-launch-1.0 -e videotestsrc is-live=true pattern=snow !\
+	video/x-raw,width=576,height=432,framerate=25/1 !\
+	x264enc psy-tune=grain qp-max=30 ! qtmux name=q !\
+	filesink location=noise.mp4\
+	audiotestsrc wave=white-noise volume=0.02 is-live=true ! voaacenc ! q.
 
