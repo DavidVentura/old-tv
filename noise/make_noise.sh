be78a53c7f1f5c41c59091d2df49d52a02b1ebb1
David <davidventura27@gmail.com>
2017-05-30 16:58:23 -0300

    make ACCURATE noise files

--- noise/make_noise.sh
+++ noise/make_noise.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-rm -f noise.mp4
-timeout -s int 5 \
-	gst-launch-1.0 -e videotestsrc is-live=true pattern=snow !\
-	video/x-raw,width=576,height=432,framerate=25/1 !\
-	x264enc psy-tune=grain qp-max=30 ! qtmux name=q !\
-	filesink location=noise.mp4\
-	audiotestsrc wave=white-noise volume=0.02 is-live=true ! voaacenc ! q.
-
+DURATION=60
+FRAMERATE=30
+DURATION=6
+gst-launch-1.0 videotestsrc pattern=snow num-buffers=$((DURATION*FRAMERATE)) ! video/x-raw,width=576,height=432,framerate=$FRAMERATE/1 ! progressreport ! x264enc psy-tune=grain qp-max=30 key-int-max=15 ! video/x-h264,profile=high ! h264parse ! mp4mux name=m ! filesink location=test.mp4 audiotestsrc wave=pink-noise num-buffers=$DURATION samplesperbuffer=44100 volume=0.1 ! audio/x-raw,channels=1,rate=44100 ! lamemp3enc ! m.
