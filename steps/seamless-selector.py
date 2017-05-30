dabee7902de668c50c3fe9c137859d1da5fc4a96
David <davidventura27@gmail.com>
2017-05-30 20:48:29 -0300

    working selector

--- steps/seamless-selector.py
+++ steps/seamless-selector.py
@@ -9,8 +9,9 @@ GObject.threads_init()
 Gst.init(None)
 
 class Player:
-    first = True
+    first = {}
     count = 0
+    duration = 0
 
     def msg(self, bus, message):
         if not message:
@@ -23,11 +24,24 @@ class Player:
 
         if t == Gst.MessageType.ASYNC_DONE:
             if self.first:
-                self.seek()
-                self.first = False
+                print("PREROLL")
+                _, d = self.pipeline.query_duration(Gst.Format.TIME)
+                self.duration = d
+                print("Duration", d/1000000000)
+                # self.seek()
+                # self.first = False
+                return
         elif t == Gst.MessageType.SEGMENT_DONE:
-            print("Looping...")
-            self.seek()
+            print("Looping... IGNORED")
+            for el in range(0, 2):
+                d = self.pipeline.get_by_name("demuxer_%d" % el)
+                _, mydur = d.query_duration(Gst.Format.TIME)
+                # print(el, d.query_duration(Gst.Format.TIME))
+                print(mydur, self.duration)
+                if mydur == self.duration:
+                    GLib.idle_add(self.seek, el)
+            # sys.exit(1)
+            # self.seek()
         elif t == Gst.MessageType.EOS:
             print("We got EOS on the pipeline.")
             sys.exit(1)
@@ -49,13 +63,13 @@ class Player:
 
         def sub_pipeline(index, filename):
             filesrc = Gst.ElementFactory.make('filesrc')
-            # f.set_property('location', "/home/david/git/old-tv/noise/test.mp4")
             filesrc.set_property('location', filename)
             demux = Gst.ElementFactory.make('qtdemux', 'demuxer_%d' % index)
             q = Gst.ElementFactory.make('queue')
             parse = Gst.ElementFactory.make('h264parse')
             dec = Gst.ElementFactory.make('avdec_h264')
-            demux.connect("pad-added", self.curry_decode_src_created(index, q, None))
+            demux.connect("pad-added",
+                          self.curry_decode_src_created(index, q, None))
 
             self.pipeline.add(filesrc)
             self.pipeline.add(demux)
@@ -68,10 +82,13 @@ class Player:
             parse.link(dec)
             dec.link(self.isv)
 
+            self.first[str(index)] = True
+
         self.isv = Gst.ElementFactory.make('input-selector')
         self.pipeline.add(self.isv)
 
         sub_pipeline(0, "/home/david/git/old-tv/noise/noise.mp4")
+        # sub_pipeline(0, "/home/david/git/old-tv/noise/test.mp4")
         sub_pipeline(1, "/home/david/git/old-tv/fast22.mp4")
 
         if platform.machine() == 'x86_64':
@@ -86,6 +103,7 @@ class Player:
         self.isvp = self.isv.request_pad(tpl_v, "sink_%u", None)
 
         self.isv.link(vsink)
+        # GLib.idle_add(self.toggle, False)
         GLib.timeout_add(2000, self.toggle)
 
 
@@ -95,16 +113,14 @@ class Player:
         if event.type == Gst.EventType.TAG:
             return Gst.PadProbeReturn.PASS
         print('event %s on pad %s', event.type, pad.get_name())
-        #if event.type == Gst.EventType.SEGMENT_DONE \
-        #or event.type == Gst.EventType.EOS:
+        # event.type == Gst.EventType.SEGMENT_DONE or 
         if event.type == Gst.EventType.EOS:
             print("Pad: %s, child of: %s" %
                   (pad.get_name(), pad.parent.get_name()))
             print("Current time:", self.get_cur_time())
-            # if pad.get_name() == "src_1" or pad.get_name() == "src_0":
             idx = pad.parent.get_name()[len('decoder_'):]
             print('Guessing idx: ', idx)
-            GLib.idle_add(self.seek)
+            GLib.idle_add(self.seek, idx)
             return Gst.PadProbeReturn.DROP
 
         return Gst.PadProbeReturn.PASS
@@ -129,16 +145,17 @@ class Player:
             if clock:
                 runtime = clock.get_time() - self.pipeline.get_base_time()
                 print("Clock! %02f" % (runtime/1000000000))
-                pad.set_offset(runtime)
+                pad.set_offset(clock.get_time() + self.duration) # FIXME?
 
             if "audio" in padname:
                 return
             elif "video" in padname:
-                # if self.p.is_linked():
-                #     print("p It's linked")
-                    # self.vsinkpad.unlink(self.vsinkpad.get_peer())
+                spad = velem.get_static_pad("sink")
+                if spad.is_linked():
+                    print("It's linked")
+                    spad.unlink(spad.get_peer())
                 print("linking video")
-                pad.link(velem.get_static_pad("sink"))
+                pad.link(spad)
 
         return decode_src_created
 
@@ -147,21 +164,29 @@ class Player:
         _, delta = self.pipeline.query_position(Gst.Format.TIME)
         return delta / 1000000000
 
-    def seek(self):
-        if self.first:
+    def seek(self, idx):
+        idx = str(idx)
+        print("seeking %s" % idx)
+        demuxer = self.pipeline.get_by_name("demuxer_%s" % idx)
+        # demuxer = self.isv.get_static_pad('src_%s' % idx)
+        if self.first[idx]:
             flags = Gst.SeekFlags.FLUSH | Gst.SeekFlags.SEGMENT
+            self.first[idx] = False
+            print("NOT FIRST!!!!!!!!!")
         else:
-            flags = Gst.SeekFlags.SEGMENT
-        self.pipeline.seek_simple(Gst.Format.TIME, flags, 0)
+            # flags = Gst.SeekFlags.SEGMENT
+            flags = Gst.SeekFlags.ACCURATE | Gst.SeekFlags.SEGMENT
+        # self.pipeline.seek_simple(Gst.Format.TIME, flags, 0)
+        demuxer.seek_simple(Gst.Format.TIME, flags, 0)
 
-    def toggle(self):
+    def toggle(self, ret=True):
         print("Togglin")
         self.count += 1
         self.count = self.count % 2
         print(self.count)
         newpad = self.isv.get_static_pad('sink_%d' % self.count)
         self.isv.set_property('active-pad', newpad)
-        return True
+        return ret
 
 loop = GObject.MainLoop()
 p = Player()
