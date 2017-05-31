97f6c03a45a3ca237aa224d912017c63d2b00932
David <davidventura27@gmail.com>
2017-05-31 16:35:09 -0300

    update gpio

--- gpio.py
+++ gpio.py
@@ -1,56 +1,44 @@
 #!/usr/bin/env python3
-import platform
-import threading
+import RPi.GPIO as GPIO
 import time
-if platform.machine() != 'x86_64':
-    import RPi.GPIO as GPIO
-
 
 class Control:
+    gpios = [4, 17, 27, 22]
     def __init__(self, cb):
-        if platform.machine() == 'x86_64':
-            return
-        GPIO.setmode(GPIO.BCM)
-        self.running = True
         self.cb = cb
-        self.gpios = [4, 17, 27, 22]
-
-        self.gpio_to_channel = {
-            -1: -1,
-            17: 2,
-            4: 4,
-            27: 5,
-            22: 8
-        }
+        GPIO.setmode(GPIO.BCM)
 
         for gpio in self.gpios:
             GPIO.setup(gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-        self.t1 = threading.Thread(target=self.control)
-        self.t1.daemon = True
-        self.t1.start()
-
-    def control(self):
-        time.sleep(0.1)
+    def start(self):
         last = -2
-        while self.running:
+        gpio_to_channel = {
+        	-1: -1,
+        	17: 2,
+        	4:  4,
+        	27: 5,
+        	22: 8
+        }
+        gpio_to_isv = {
+        	-1: 0,
+        	17: 1,
+        	4:  2,
+        	27: 3,
+        	22: 4
+        }
+        while True:
+            valid = False
             value = -1
             for g in self.gpios:
                 v = GPIO.input(g)
                 if v:
                     value = g
             if value != last:
-                self.cb(self.gpio_to_channel[value])
+                self.cb(gpio_to_isv[value])
                 last = value
             time.sleep(0.05)
 
-    def stop(self):
-        if platform.machine() == 'x86_64':
-            return
-        self.running = False
-        print("waiting to join")
-        self.t1.join()
-
-
 if __name__ == '__main__':
     c = Control(print)
+    c.start()
